cf10899c565d7d85fc1c90d1304ddbf107783d2e
Aleksandr Poryadin <a.poryadin@tinkoff.ru>
2024-05-25 15:33:01 +0300

    add payment

--- bochka/orm/models/images.py
+++ bochka/orm/models/images.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy_imageattach.entity import Image
 from bochka.orm.baseclass import BaseClass
 from bochka.orm.models.hotel import Hotel
+from bochka.orm.models.payment import PaymentReceipt
 from bochka.orm.models.user import User
 
 
@@ -19,3 +20,8 @@ class RoomImage(BaseClass, Image):
 class UserImage(BaseClass, Image):
     user_id = Column(UUID, User.id, nullable=False)
     user = relationship("User", back_populates="user_images", cascade_backrefs=False)
+
+
+class ReceiptImage(BaseClass, Image):
+    receipt_id = Column(UUID, PaymentReceipt.id, nullable=False)
+    receipt = relationship("PaymentReceipt", back_populates="receipt_images", cascade_backrefs=False)
--- bochka/orm/models/payment.py
+++ bochka/orm/models/payment.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, UUID, DateTime, String, Float
+from sqlalchemy_imageattach.entity import image_attachment
 
 from bochka.orm.baseclass import BaseClass
 from bochka.orm.models.booking import Booking
@@ -10,3 +11,9 @@ class Payment(BaseClass):
     amount = Column(Float, nullable=False)
     payment_date = Column(DateTime, nullable=False)
     status = Column(String, nullable=False)
+
+
+class PaymentReceipt(BaseClass):
+    payment_id = Column(UUID, Payment.id, nullable=False)
+
+    picture = image_attachment("ReceiptImage")
